166517c1fc23a4ab8131092a7121a3298247230e
moasjogren <moasjgren@gmail.com>
2025-03-28 14:26:22 +0100

    Add CORS support and user/job routes to the server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes";
+import jobRoutes from "./routes/jobRoutes";
 
 dotenv.config();
 
@@ -8,7 +11,11 @@ const app = express();
 const PORT = Number(process.env.PORT);
 
 // Middleware
+app.use(cors());
 app.use(express.json());
+
+app.use("/users", userRoutes);
+app.use("/jobs", jobRoutes);
 
 // Health check endpoint
 app.get("/", (req, res) => {
@@ -17,6 +24,10 @@ app.get("/", (req, res) => {
     port: PORT,
     timestamp: new Date().toISOString(),
   });
+});
+
+app.get("/hej", (req, res) => {
+  res.send("Hejsan!");
 });
 
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
