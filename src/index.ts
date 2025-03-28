6e7b337b84766bfdb294738c64674255a76ce3ab
moasjogren <moasjgren@gmail.com>
2025-03-28 09:24:42 +0100

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes";
+import jobRoutes from "./routes/jobRoutes";
 
 const app = express();
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/users", userRoutes);
+app.use("/jobs", jobRoutes);
+
 app.get("/", (req, res) => {
-  res.send("Node.js and Express.js with TypeScript");
+  res.send(`Server JobChaser. On port: ${port}`);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
